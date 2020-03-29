dc3113acbece4d7dccdcd9898e9b12fa28d99b4b
kosakatu <kskktk0225@gmail.com>
2020-03-29 16:26:46 +0900

    Refactoring MemoList

---
+++ src/elements/CricleButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet,View,Text} from 'react-native';
+
+class CricleButton extends React.Component {
+  render(){
+    return(
+      <View style={styles.cricleButton}>
+        <Text style={styles.cricleButtonTitle}>
+        { this.props.children }
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cricleButton:{
+    position:'absolute',
+    bottom:32,
+    right:32,
+    width:48,
+    height:48,
+    backgroundColor:'#E31676',
+    borderRadius:24,
+    justifyContent:'center',
+    alignItems:'center',
+    shadowColor:'#000',
+    shadowOffset:{width:0,height:2},
+    shadowOpacity:0.2,
+    shadowRadius:3,
+  },
+  cricleButtonTitle:{
+    fontSize:32,
+    lineHeight:32,
+    color:'#fff',
+
+  },
+});
+
+export default CricleButton;
